221d76689d36bf9944fe50ee695983ee706b0e0d
lm2221 <lm2221@columbia.edu>
2017-02-28 22:40:20 -0500

    new features beginning script

--- wrangling/merge_queries.py
+++ wrangling/merge_queries.py
@@ -7,9 +7,11 @@ all_files = []
 dir = '../data/large_data_store/'
 for i, filename in enumerate(os.listdir(dir)):
     if 'join' in filename:
+        print 'File Number:', i
         all_files.extend(joblib.load(filename=os.path.join(dir, filename)))
 column_names = joblib.load(os.path.join(dir,'column_names'))
 
+print 'Saving file to csv'
 all_posts = pd.DataFrame(all_files)
 all_posts.columns = column_names
 all_posts.to_csv('../data/all_posts.csv',
